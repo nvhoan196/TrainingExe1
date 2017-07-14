6f847c1ef2695504352f699661a23f675444ac3c
mac <mac@macs-MacBook-Pro.local>
2017-07-14 12:09:32 +0700

    no message

---
+++ TrainingExe1/ViewController2.swift
@@ -0,0 +1,53 @@
+//
+//  ViewController2.swift
+//  TrainingExe1
+//
+//  Created by mac on 7/14/17.
+//  Copyright © 2017 mac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController2: UIViewController {
+    
+    
+    
+    @IBOutlet weak var txtName: UITextField!
+    
+    @IBOutlet weak var txtAge: UITextField!
+    
+    @IBOutlet weak var txtAdress: UITextField!
+
+    @IBOutlet weak var txtPhone: UITextField!
+    
+    @IBOutlet weak var Reprort: UILabel!
+    
+    @IBOutlet weak var ViewTT: UITextView!
+    
+    @IBAction func ADD(_ sender: Any) {
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
